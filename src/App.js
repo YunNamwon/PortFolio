40391cc6d0db63e89ccbe0dcfaf52c3ebb7c6f84
YunNamwon <109219153+YunNamwon@users.noreply.github.com>
2023-12-12 15:22:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Themes";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/HeroSection";
+import { Skills } from "./components/Skills"
+import { Education } from "./components/Education";
+import { BrowserRouter as Router } from "react-router-dom"
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+    38.73deg, 
+    rgba(204, 0, 187, 0.15) 0%, 
+    rgba(201, 32, 184, 0) 50%
+    ), 
+    linear-gradient(
+      141.27deg, 
+      rgba(0, 70, 209, 0) 50%, 
+      rgba(0, 70, 209, 0.15) 100%
+      );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+      <Navbar />
+      <Body>
+        <Hero />
+        <Wrapper>
+          <Skills />
+          <Education />
+        </Wrapper>
+      </Body>
+      </Router>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+
+// Wrapper 코드는 background 속성으로 두 개의 linear-gradient를 정의하고 있습니다. 각 linear-gradient는 색상의 그라데이션 효과를 생성합니다.
+// 첫 번째 linear-gradient:
+// 각도(38.73deg)
+// 시작 색상(rgba(204, 0, 187, 0.15))에서 끝 색상(rgba(201, 32, 184, 0))까지의 그라데이션을 나타냅니다.
+// 그라데이션의 50% 지점에서 색상이 투명해집니다.
+// 두 번째 linear-gradient:
+
+// 각도(141.27deg)
+// 시작 색상(rgba(0, 70, 209, 0))에서 끝 색상(rgba(0, 70, 209, 0.15))까지의 그라데이션을 나타냅니다.
+// 그라데이션의 50% 지점에서 색상이 투명해집니다.
+// 이후에는 width 속성으로 너비를 100%로 설정하고, 
+// clip-path 속성을 사용하여 다각형 모양의 클리핑 경로를 정의하고 있습니다. 
+// polygon() 함수는 다각형의 꼭지점을 정의하며, 이를 통해 요소의 모양을 정의할 수 있습니다. 
+// 위 코드에서는 왼쪽 위, 오른쪽 위, 오른쪽 아래, 왼쪽 아래, 중간 아래 5개의 꼭지점을 지정하여 다각형 모양을 만들어냅니다.
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#1C1C27",         // 배경 색상
+  bgLight: "#1C1E27",    // 밝은 배경 색상
+  primary: "#854CE6",    // 주요 색상
+  text_primary: "#F2F3F4", // 기본 텍스트 색상
+  text_secondary: "#b1b2b3", // 보조 텍스트 색상
+  card: "#171721",       // 카드 배경 색상
+  card_light: "#191924",  // 밝은 카드 배경 색상
+  button: "#854CE6",     // 버튼 색상
+  white: "#FFFFFF",      // 흰색
+  black: "#000000",      // 검은색
+}
+
+export const lightTheme = {
+  bg: "#FFFFFF",         // 배경 색상
+  bgLight: "#f0f0f0",    // 밝은 배경 색상
+  primary: "#be1adb",    // 주요 색상
+  text_primary: "#111111", // 기본 텍스트 색상
+  text_secondary: "#48494a", // 보조 텍스트 색상
+  card: "#FFFFFF",       // 카드 배경 색상
+  button: "#5c5b5b",     // 버튼 색상
+}
